28f841d250a5c8d87f85ba09c63cd8f2daa66864
Chukseo <164390660+Chukseo@users.noreply.github.com>
2025-02-14 14:56:26 +0100

    deploy

--- userapi/settings.py
+++ userapi/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-plks-)42g+66nsuu!lf+ldb@lt8-*a0r)vd+c1*$2#)!u+d*-l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
