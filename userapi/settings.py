b2a25012ec107e8d07bcba5c15e9a1e1c36732e9
Chukseo <164390660+Chukseo@users.noreply.github.com>
2025-02-21 15:40:05 +0100

    debug

--- userapi/settings.py
+++ userapi/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-plks-)42g+66nsuu!lf+ldb@lt8-*a0r)vd+c1*$2#)!u+d*-l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','https://peakuser.onrender.com''peakuser.onrender.com']
+ALLOWED_HOSTS = ['localhost','https://peakuser.onrender.com','peakuser.onrender.com']
 
 
 # Application definition
