f49f4d496db656d33ab8ed25c2b4c12e536ab96e
Chukseo <164390660+Chukseo@users.noreply.github.com>
2025-02-21 15:31:48 +0100

    debug

--- userapi/settings.py
+++ userapi/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-plks-)42g+66nsuu!lf+ldb@lt8-*a0r)vd+c1*$2#)!u+d*-l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','https://peakuser.onrender.com']
+ALLOWED_HOSTS = ['localhost','https://peakuser.onrender.com''peakuser.onrender.com']
 
 
 # Application definition
